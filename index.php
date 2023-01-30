f59008a951801ac1495ff7b2d30b8f4dc000cfb7
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-01-30 15:14:31 +0100

    connection to db

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<?php
+if (isset($_POST['name']) && isset($_POST['firstname']) && isset($_POST['email']) && isset($_POST['comment'])) {
+    $name = $_POST['name'];
+    $firstname = $_POST['firstname'];
+    $email = $_POST['email'];
+    $comment = $_POST['comment'];
+    try {
+        $db = new PDO('mysql:host=localhost;dbname=hackers_poulette;charset=utf8', 'root', '');
+
+        $insertValue = "INSERT INTO form (name, firstname, email, comment) VALUES (?, ?, ?, ?)";
+
+        $insert = $db->prepare($insertValue);
+
+        $insert->execute([$name, $firstname, $email, $comment]);
+    } catch (\Throwable $th) {
+        echo 'error' . $th->getMessage();
+    }
+    header("location: index.php");
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,22 +31,21 @@
 
 <body>
     <h1>Contactez nous</h1>
-    <form>
+    <form method="post">
         <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required minlength="2" maxlength="255">
+        <input type="text" id="name" name="name" required minlength="2" maxlength="255" placeholder="ex. Doe">
         <br>
         <label for="firstname">Firstname:</label>
-        <input type="text" id="firstname" name="firstname" required minlength="2" maxlength="255">
+        <input type="text" id="firstname" name="firstname" required minlength="2" maxlength="255" placeholder="ex. John">
         <br>
         <label for="email">Email Address:</label>
-        <input type="email" id="email" name="email" required minlength="2" maxlength="255">
+        <input type="email" id="email" name="email" required minlength="2" maxlength="255" placeholder="ex. john.doe@example.com">
         <br>
         <label for="comment">Comment:</label>
-        <textarea id="comment" name="comment" required minlength="2" maxlength="1000"></textarea>
+        <textarea id="comment" name="comment" required minlength="1" maxlength="1000"></textarea>
         <br>
-        <input type="submit" value="Submit">
+        <input type="submit" value="Submit" name="submit">
     </form>
-
 </body>
 
 </html>
